9674ef169075a2824fd76497055a5a891f1d2f9a
Paulo Mogollón <pmogollon@propiedata.com>
2023-04-14 06:25:14 -0500

    On Meteor.call return promise if no callback param

--- src/Call.js
+++ src/Call.js
@@ -8,12 +8,24 @@ import Data from './Data';
 export default function (eventName) {
   var args = Array.prototype.slice.call(arguments, 1);
   if (args.length && typeof args[args.length - 1] === 'function') {
-    var callback = args.pop();
-  }
+    const callback = args.pop();
+    const id = Data.ddp.method(eventName, args);
 
-  const id = Data.ddp.method(eventName, args);
-  Data.calls.push({
-    id: id,
-    callback: callback,
-  });
+    Data.calls.push({ id: id, callback });
+  } else {
+    const id = Data.ddp.method(eventName, args);
+
+    return new Promise((resolve, reject) => {
+      Data.calls.push({
+        id: id,
+        callback(error, response) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(response);
+          }
+        },
+      });
+    });
+  }
 }
